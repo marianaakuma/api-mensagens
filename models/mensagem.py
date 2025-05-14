fdf75c69384a0e77b447e58a8e5fc6159f64bc1c
marianaakuma <153663415+marianaakuma@users.noreply.github.com>
2025-05-14 14:43:19 +0000

    14/05/2025

--- models/mensagem.py
+++ models/mensagem.py
@@ -1,7 +1,7 @@
 class mensagem(db.Model, UserMixin):
   __tablename__="mensagem"
   id = db.Column(db.Integer, primary_key=True)
-    mensagem = db.Column(db.String(100))
+  mensagem = db.Column(db.String(100))
   
 
   def __init__(self,mensagem,):
